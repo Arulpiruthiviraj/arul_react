6aba3e6d15114f778f830654f28156401a4ca613
Arulpiruthiviraj <arul.ceymplon@jeylabs.com.au>
2019-07-22 14:31:10 +0530

    animation added

--- src/components/Skill.js
+++ src/components/Skill.js
@@ -4,7 +4,7 @@ import { skillsDataFrameworks } from "./skillData"
 import { skillsDataLanguages } from "./skillData"
 import { skillsDataLearning } from "./skillData"
 import {Container,Row,Col,Card} from "react-bootstrap";
-
+import "../assets/css/LogoAnime.css"
 
 function Skills() {
     const [cards,setCards]=useState([]);
@@ -25,7 +25,6 @@ function Skills() {
                     <br/>
                 </Col>
             </Row>
-
             <div className="row ">
                 { skillsDataFrameworks.map((element, index) => (
                         <div
@@ -35,7 +34,9 @@ function Skills() {
                         >
                             <Card bg="secondary" text="white" style={{ width: '18rem' ,height:"18rem"}} >
                                 <Card.Header>{element.name}</Card.Header>
-                                <Card.Link href={element.framework_link} target={"_blank"}><Card.Img variant="top" src={element.logo} height={100} width={100}/></Card.Link>
+                                <Card.Link href={element.framework_link} target={"_blank"}>
+                                    <Card.Img variant="top" className={element.anime_class} alt="logo" src={element.logo} height={100} width={100}/>
+                                </Card.Link>
                                 <Card.Body >
                                     <Card.Text  >
                                         {element.secondaryTitle}
@@ -46,59 +47,37 @@ function Skills() {
                         </div>
                     ))}
             </div>
+            <br/>
+            <Row>
+                <Col xs={12} md={1}>
+                    <Card bg="secondary" text="white" style={{ width: '50rem' }}>
+                        <Card.Header>Frameworks and Technologies</Card.Header>
+                    </Card>
+                    <br/>
+                </Col>
+            </Row>
+            <br/>
             <div className="row ">
-                { skillsDataFrameworks.map((element, index) => (
+                { skillsDataLanguages.map((element, index) => (
                         <div
                             key={element.id}
                             className="col-12 col-sm-6 col-md-4 card-element"
                             ref={div => cards[index] = div}
                         >
-                            <div className="card mt-3">
-                                <div className="card-body">
-                                    <div className="media">
-                                        <img
-                                            className="mr-3"
-                                            src="https://via.placeholder.com/64"
-                                            alt="Generic placeholder image"
-                                        />
-                                        <div className="media-body">
-                                            <h5 className="mt-0">{element.name}</h5>
-                                        </div>
-                                    </div>
-                                </div>
-                            </div>
-                            <br/>
+                            <Card bg="secondary" text="white" style={{ width: '18rem' ,height:"18rem"}} >
+                                <Card.Header>{element.name}</Card.Header>
+                                <Card.Link href={element.framework_link} target={"_blank"}>
+                                    <Card.Img variant="top" className={element.anime_class} alt="logo" src={element.logo} height={100} width={100}/>
+                                </Card.Link>
+                                <Card.Body >
+                                    <Card.Text  >
+                                        {element.secondaryTitle}
+                                    </Card.Text>
+                                </Card.Body>
+                            </Card>
                             <br/>
                         </div>
                     ))}
-
-            </div>
-            <div className="row ">
-                { skillsDataLearning.map((element, index) => (
-                        <div
-                            key={element.id}
-                            className="col-12 col-sm-6 col-md-4 card-element"
-                            ref={div => cards[index] = div}
-                        >
-                            <div className="card mt-3">
-                                <div className="card-body">
-                                    <div className="media">
-                                        <img
-                                            className="mr-3"
-                                            src="https://via.placeholder.com/64"
-                                            alt="Generic placeholder image"
-                                        />
-                                        <div className="media-body">
-                                            <h5 className="mt-0">{element.name}</h5>
-                                        </div>
-                                    </div>
-                                </div>
-                            </div>
-                            <br/>
-                            <br/>
-                        </div>
-                    ))}
-
             </div>
         </Container>
 
--- src/components/skillData.js
+++ src/components/skillData.js
@@ -16,39 +16,50 @@ export const skillsDataFrameworks = [
         "logo":laravelLogo,
         "secondaryTitle":" Laravel is an open source web development  framework which is written in PHP ." +
             "Laravel provides a rich set of functionalities similar to that of Ruby on Rails.",
-        "framework_link":"https://laravel.com/"
+        "framework_link":"https://laravel.com/",
+        "anime_class":"laravel-logo"
+
     },
     {
         "name": "Django",
         "logo":djangoLogo,
         "secondaryTitle":" Django is a high-level open source web framework based on Python that provides rapid development and clean design solution",
-        "framework_link":"https://www.djangoproject.com/"
+        "framework_link":"https://www.djangoproject.com/",
+        "anime_class":"django-logo"
     },
     {
         "name": "ReactJS",
         "logo":reactLogo,
         "secondaryTitle":" React is a JavaScript library for building user interfaces.React can be used as a base in the development of single-page ",
-        "framework_link":"https://reactjs.org/"
+        "framework_link":"https://reactjs.org/",
+        "anime_class":"react-logo"
     },
     {
         "name": "AngularJS",
         "logo":angularLogo,
         "secondaryTitle":"AngularJS is a JavaScript-based open-source front-end web framework mainly maintained by Google",
-        "framework_link":"https://angularjs.org/"
+        "framework_link":"https://angularjs.org/",
+        "anime_class":"django-logo"
+
     },
    {
         "name": "jQuery",
         "logo":jqueryLogo,
         "secondaryTitle":"jQuery is a JavaScript library designed to simplify HTML DOM tree traversal and manipulation, as well as event handling, CSS animation, and Ajax.",
-        "framework_link":"https://jquery.com/"
-    },
+        "framework_link":"https://jquery.com/",
+       "anime_class":"django-logo"
+
+   },
    {
         "name": "Bootstrap",
         "logo":bootstrapLogo,
         "secondaryTitle":"Bootstrap is a free and open-source CSS framework directed at responsive, " +
             "mobile-first front-end web development.",
-        "framework_link":"https://getbootstrap.com/"
-    },
+        "framework_link":"https://getbootstrap.com/",
+       "anime_class":"django-logo"
+
+
+   },
 
 ];
 export const skillsDataLanguages = [
