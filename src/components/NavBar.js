40d91a9656bd10921cca689eecfb905d55fce0c8
Arul <aprprasanth16@gmail.com>
2020-10-30 05:38:08 -0400

    changes

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,46 +1,40 @@
-import React from 'react';
-import { NavLink} from "react-router-dom";
-import {Nav, Navbar} from "react-bootstrap";
-import portFolioSvg from '../assets/images/portfolio.svg'
+import React from "react";
+import { NavLink } from "react-router-dom";
+import { Nav, Navbar } from "react-bootstrap";
+import portFolioSvg from "../assets/images/portfolio.svg";
 
-
-function NavBar({routes}) {
-    return (
-        <Navbar collapseOnSelect expand="lg" bg="dark" variant="dark"  fixed="top">
-            <Navbar.Brand
-                as={NavLink}
-                to={'/'}
-                activeClassName="active"
-                exact>
-                <img
-                    alt=""
-                    src={portFolioSvg}
-                    width="30"
-                    height="30"
-                    className="d-inline-block align-top"
-                />
-                &nbsp;&nbsp;&nbsp;&nbsp;
-                {'Arulpiruthiviraj'}
-            </Navbar.Brand>
-            <Navbar.Toggle aria-controls="responsive-navbar-nav" />
-            <Navbar.Collapse id="responsive-navbar-nav" >
-                <Nav className="">
-                    {routes.map(route => (
-                        <Nav.Link
-                            key={route.path}
-                            as={NavLink}hj
-                            to={route.path}
-                            activeClassName="active"
-                            exact
-                        >
-                            {route.name}
-                        </Nav.Link>
-                    ))}
-                </Nav>
-            </Navbar.Collapse>
-        </Navbar>
-    );
-
+function NavBar({ routes }) {
+  return (
+    <Navbar collapseOnSelect expand="lg" bg="dark" variant="dark" fixed="top">
+      <Navbar.Brand as={NavLink} to={"/"} activeClassName="active" exact>
+        <img
+          alt=""
+          src={portFolioSvg}
+          width="30"
+          height="30"
+          className="d-inline-block align-top"
+        />
+        &nbsp;&nbsp;&nbsp;&nbsp;
+        {"Arulpiruthiviraj"}
+      </Navbar.Brand>
+      <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+      <Navbar.Collapse id="responsive-navbar-nav">
+        <Nav className="">
+          {routes.map((route) => (
+            <Nav.Link
+              key={route.path}
+              as={NavLink}
+              to={route.path}
+              activeClassName="active"
+              exact
+            >
+              {route.name}
+            </Nav.Link>
+          ))}
+        </Nav>
+      </Navbar.Collapse>
+    </Navbar>
+  );
 }
 
 export default NavBar;
--- src/routes/routes.js
+++ src/routes/routes.js
@@ -1,50 +1,51 @@
-import React from 'react'
-import { BrowserRouter as Router, Route} from 'react-router-dom'
-import { CSSTransition } from 'react-transition-group'
-import { Container} from 'react-bootstrap'
-import '../assets/css/style.css'
+import React from "react";
+import { BrowserRouter as Router, Route } from "react-router-dom";
+import { CSSTransition } from "react-transition-group";
+import { Container } from "react-bootstrap";
+import "../assets/css/style.css";
 import Home from "../components/Home";
 import About from "../components/About";
 import Contact from "../components/Contact";
 import NavBar from "../components/NavBar";
 import TimeLine from "../components/TimeLine";
 import Skills from "../components/Skill";
+import ScrollToTop from "../components/ScrollToTop";
 
 const routes = [
-    { path: '/', name: 'Home', Component: Home },
-    { path: '/skills', name: 'Skills', Component: Skills },
-    { path: '/timeline', name: 'Timeline', Component: TimeLine },
-    { path: '/blog', name: 'Blog', Component: About },
-    { path: '/contact', name: 'Contact', Component: Contact},
+  { path: "/", name: "Home", Component: Home },
+  { path: "/skills", name: "Skills", Component: Skills },
+  { path: "/timeline", name: "Timeline", Component: TimeLine },
+  { path: "/contact", name: "Contact", Component: Contact },
 ];
 
 export default function Routes() {
-    return (
-        <Router>
-            <>
-                <Container>
-                    <NavBar routes={routes}/>
-                </Container>
-                <Container className="container">
-                    {routes.map(({ path, Component }) => (
-                        <Route key={path} exact path={path}>
-                            {({ match }) => (
-                                <CSSTransition
-                                    in={match != null}
-                                    timeout={300}
-                                    classNames="page"
-                                    unmountOnExit
-                                >
-                                    <div className="page">
-                                        <Component />
-                                    </div>
-                                </CSSTransition>
-                            )}
-                        </Route>
-                    ))}
-                </Container>
-            </>
-        </Router>
-    )
-}
+  return (
+    <Router>
+      <>
+        <ScrollToTop />
 
+        <Container>
+          <NavBar routes={routes} />
+        </Container>
+        <Container className="container">
+          {routes.map(({ path, Component }) => (
+            <Route key={path} exact path={path}>
+              {({ match }) => (
+                <CSSTransition
+                  in={match != null}
+                  timeout={300}
+                  classNames="page"
+                  unmountOnExit
+                >
+                  <div className="page">
+                    <Component />
+                  </div>
+                </CSSTransition>
+              )}
+            </Route>
+          ))}
+        </Container>
+      </>
+    </Router>
+  );
+}
